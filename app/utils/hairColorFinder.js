856c9f720b5d85b16f88c7ee167ca585c4d2fc9e
Agustin Lemes <86081166+iAgis@users.noreply.github.com>
2025-02-02 21:28:19 -0300

    Update hairColorFinder.js

--- app/utils/hairColorFinder.js
+++ app/utils/hairColorFinder.js
@@ -46,217 +46,97 @@ function mixColors(color1, color2) {
 
   return { primary: yRes, secondary: zRes };
 }
-// BEHAVIOR EXPECTED START
-const primarios = [1, 6, 3]; // la mezcla de 2 primarios da un secundario
-const secundarios = [7, 4, 2]; // la mezcla de 2 secundarios da un ternario
-const terniarios = [8, 9, 5]; // fin de la sucecion. No hay cuaternarios
-const neutro = [0]; // color neutro = marron
 
-const matrixShades = [primarios, secundarios, terniarios, neutro];
-const matrixShadeMixer = [
-  [1, 6, 3],
-  [4, 7, 2],
+const colores = [
+  { numero: 0, color: "Marrón", grados: 0, profundidad: 0 }, // Neutro
+  { numero: 1, color: "Azul", grados: 0, profundidad: 1 },
+  { numero: 6, color: "Rojo", grados: 240, profundidad: 1 },
+  { numero: 3, color: "Amarillo", grados: 120, profundidad: 1 },
+  { numero: 7, color: "Verde", grados: 60, profundidad: 2 },
+  { numero: 4, color: "Naranja", grados: 180, profundidad: 2 },
+  { numero: 2, color: "Violeta", grados: 300, profundidad: 2 },
+  { numero: 5, color: "Terciario", grados: 240, profundidad: 3 },
+  { numero: 8, color: "Terciario", grados: 60, profundidad: 3 },
+  { numero: 9, color: "Terciario", grados: 120, profundidad: 3 },
+  // Se agregan los colores sumados en secuencia
+  { numeros: 71, color: "17 cenizo mate", grados: 30, profundidad: 2.5 },
+  { numero: 17, color: "17 cenizo mate", grados: 30, profundidad: 2.5 },
+  { numero: 73, color: "31 beige", grados: 90, profundidad: 2.5 },
+  { numero: 37, color: "31 beige", grados: 90, profundidad: 2.5 },
+  { numero: 34, color: "4 naranja o cobre", grados: 150, profundidad: 2.5 },
+  { numero: 43, color: "4 naranja o cobre", grados: 150, profundidad: 2.5 },
+  { numero: 46, color: "6 rojo", grados: 210, profundidad: 2.5 },
+  { numero: 64, color: "6 rojo", grados: 210, profundidad: 2.5 },
+  { numero: 62, color: "62 rojo violin", grados: 270, profundidad: 2.5 },
+  { numero: 26, color: "62 rojo violin", grados: 270, profundidad: 2.5 },
+  { numero: 21, color: "12 cenizo nacarado", grados: 330, profundidad: 2.5 },
+  { numero: 12, color: "12 cenizo nacarado", grados: 330, profundidad: 2.5 },
+  { numero: 13, color: "7 mate o verde", grados: 60, profundidad: 2.5 },
+  { numero: 31, color: "7 mate o verde", grados: 60, profundidad: 2.5 },
+  { numero: 16, color: "2 nacarado o violeta", grados: 300, profundidad: 2.5 },
+  { numero: 61, color: "2 nacarado o violeta", grados: 300, profundidad: 2.5 },
+  { numero: 36, color: "4 naranja o cobre", grados: 180, profundidad: 2.5 },
+  { numero: 63, color: "4 naranja o cobre", grados: 180, profundidad: 2.5 },
+  { numero: 72, color: "8 moka o marrón cenizo", grados: 0, profundidad: 2.5 },
+  { numero: 27, color: "8 moka o marrón cenizo", grados: 0, profundidad: 2.5 },
+  {
+    numero: 24,
+    color: "5 caoba o marron rojizo",
+    grados: 240,
+    profundidad: 2.5,
+  },
+  {
+    numero: 42,
+    color: "5 caoba o marron rojizo",
+    grados: 240,
+    profundidad: 2.5,
+  },
 ];
 
-const MixedByMatrixIndex =
-  matrixShadeMixer[0][0].toString() + matrixShadeMixer[1][1].toString();
-console.log(MixedByMatrixIndex);
-
-switch (MixedByMatrixIndex) {
-  case "71":
-  case "17":
-    return console.log("17 cenizo mate");
-  case "12":
-  case "21":
-    return console.log("12 cenizo nacarado");
-  case "64":
-  case "46":
-    return console.log("6 rojo");
-  case "62":
-  case "26":
-    return console.log("62 rojo violin");
-  case "34":
-  case "43":
-    return console.log("4 naranja o cobre");
-  case "37":
-  case "73":
-    return console.log("31 beige");
-  case "16":
-  case "61":
-    return console.log("2 nacarado o violeta");
-  case "13":
-  case "31":
-    return console.log("7 mate o verde");
-  case "36":
-  case "63":
-    return console.log("4 naranja o cobre");
-  case "72":
-  case "27":
-    return console.log("8 moka o marrón cenizo");
-  case "24":
-  case "42":
-    return console.log("5 caoba o marron rojizo");
-  default:
-    return console.log("0 marron");
+// Función para obtener un color por su número
+function getColor(numero) {
+  return colores.find((c) => c.numero === numero);
 }
-// BEHAVIOR EXPECTED END
 
-// Colorimetria
-const colorimetriaMatrix = {
-  basic: [1, 7, 3, 4, 6, 2],
-  neutros: [4, 6, 2, 1, 7, 3], // indice de basic y mismo indice con el que se mezcla = 0
-  secundarios: [3, 4, 6, 2, 1, 7], // indice de basic y mismo indice con el que se mezcla = indice de secundariosRes
-  secundariosRes: [7, 9, 4, 5, 2, 8], // Resultados entre basic y secundarios
-  // TODO: Revisar los valores de los ternarios y su comportamiento
-  yernarios: [7, 3, 4, 6, 2, 1], // indice de basic y mismo indice con el que se mezcla = indice de yernariosRes
-  // TODO: Revisar
-  yernariosRes: [0.17, 0.73, 0.4, 0.6, 0.62, 0.21], // Resultados entre basic y ternarios
-  // TODO: Revisar
-  yernariosInvertRes: [0.71, 0.37, 0.4, 0.6, 0.26, 0.12], // Resultados entre ternarios y basic
-  yernarios2: [2, 1, 7, 3, 4, 6],
-  // TODO: Revisar
-  yernariosRes2: [0.26, 0.12, 0.71, 0.37, 0.6, 0.64],
-  // TODO: Revisar
-  yernariosInvertRes2: [0.62, 0.21, 0.17, 0.73, 0.6, 0.46],
-  // t de Tercearios
-  t: [8, 9, 5],
-  tNeutros: [4, 2, 7],
-  tNeutrosRes: [0.04, 0.02, 0.07],
-  tSecundarios: [3, 6, 1],
-  tSecundariosRes: ["1", "1", "1"], // TODO: Las X estan para poner valores que no se han calculado
-  tSecundarios2: [6, 1, 3],
-  tSecundariosRes: ["1", "1", 0.05], // TODO: Las X estan para poner valores que no se han calculado
-  tYernarios: [7, 4, 2],
-  tYernariosRes: ["1", "1", "1"], // TODO: Las X estan para poner valores que no se han calculado
-  tYernarios2: [2, 7, 4],
-  tYernariosRes: ["1", "1", 0.05], // TODO: Las X estan para poner valores que no se han calculado
-  tMismos: [1, 3, 6],
-  tMismosRes: ["1", "1", "1"], // TODO: Las X estan para poner valores que no se han calculado
-};
-
-/**
- * @param {number} highlights1
- * @param {number} highlights2
- * @returns {number} The sum of both highlights
- * @description Given two highlights, returns the sum of both
- */
+// Función para sumar colores
 function plusHighlights(highlights1, highlights2) {
-  highlights1 = parseFloat(highlights1);
-  highlights2 = parseFloat(highlights2);
+  const hl1 = parseFloat(highlights1);
+  const hl2 = parseFloat(highlights2);
 
-  // If some is 0, return the other
-  if (!highlights1) return highlights2;
-  if (!highlights2) return highlights1;
+  if (!hl1) return hl2;
+  if (!hl2) return hl1;
+  if (hl1 === hl2) return hl2;
 
-  // If both are the same, return first
-  if (highlights1 === highlights2) return highlights1;
+  const color1 = getColor(hl1);
+  const color2 = getColor(hl2);
+  const neutro = getColor(0);
 
-  // Search index of highlights1 in basic
-  const indexBasic = colorimetriaMatrix.basic.indexOf(highlights1);
-  const indexBasic2 = colorimetriaMatrix.basic.indexOf(highlights2);
-  const indexBasicTerciarios = colorimetriaMatrix.t.indexOf(highlights1);
-  const indexBasicTerciarios2 = colorimetriaMatrix.t.indexOf(highlights2);
+  // Caso: opuestos en grados (neutro)
+  if (!color1) throw new Error("No se encontro el color para el: " + hl1);
+  if (!color2) throw new Error("No se encontro el color para el: " + hl2);
+  if (Math.abs(color1.grados - color2.grados) === 180) return neutro.numero;
 
-  const hasTerciario =
-    indexBasicTerciarios !== -1 || indexBasicTerciarios2 !== -1;
+  // Calcular la media de los grados (círculo)
+  const gradosResultado = (color1.grados + color2.grados) / 2;
 
-  // console.log({
-  //   indexBasic,
-  //   indexBasic2,
-  //   indexBasicTerciarios,
-  //   indexBasicTerciarios2,
-  //   hasTerciario,
-  // });
+  // Resolver conflictos por profundidad
+  const coloresCandidatos = colores.filter((c) => c.grados === gradosResultado);
+  const colorResultado = coloresCandidatos.reduce((mejor, actual) => {
+    return actual.profundidad > (mejor?.profundidad || 0) ? actual : mejor;
+  }, null);
 
-  if (!hasTerciario) {
-    const isBasic = indexBasic !== -1;
-    if (!isBasic) throw new Error("Invalid highlights1, not found in matrix");
-
-    // Is Neutro highlights2?
-    const neutro = colorimetriaMatrix.neutros[indexBasic];
-    if (highlights2 === neutro) return 0; // Los neutros se anulan
-    // Lo mismo pero al reves
-    const neutro2 = colorimetriaMatrix.neutros[indexBasic2];
-    if (highlights1 === neutro2) return 0; // Los neutros se anulan
-
-    // Is Mix highlights2 with basic?
-    const basicCon = colorimetriaMatrix.secundarios[indexBasic];
-    if (highlights2 === basicCon)
-      return colorimetriaMatrix.secundariosRes[indexBasic];
-    // Lo mismo pero al reves
-    const basicCon2 = colorimetriaMatrix.secundarios[indexBasic2];
-    if (highlights1 === basicCon2)
-      return colorimetriaMatrix.secundariosRes[indexBasic2];
-
-    // Is Mix highlights2 with yernarios?
-    const yernario = colorimetriaMatrix.yernarios[indexBasic];
-    if (highlights2 === yernario)
-      return colorimetriaMatrix.yernariosRes[indexBasic];
-    // Lo mismo pero al reves
-    const yernario2 = colorimetriaMatrix.yernarios[indexBasic2];
-    if (highlights1 === yernario2)
-      return colorimetriaMatrix.yernariosInvertRes[indexBasic2];
-
-    // Is Mix highlights2 with yernarios2?
-    const yernario2Fila2 = colorimetriaMatrix.yernarios2[indexBasic];
-    if (highlights2 === yernario2Fila2)
-      return colorimetriaMatrix.yernariosRes2[indexBasic];
-    // Lo mismo pero al reves
-    const yernarioFila2 = colorimetriaMatrix.yernarios2[indexBasic2];
-    if (highlights1 === yernarioFila2)
-      return colorimetriaMatrix.yernariosInvertRes2[indexBasic2];
-  } else {
-    // Is Terciario highlights2?
-    const terciario = colorimetriaMatrix.tNeutros[indexBasicTerciarios];
-    if (highlights2 === terciario)
-      return colorimetriaMatrix.tNeutrosRes[indexBasicTerciarios];
-    // Lo mismo pero al reves
-    const terciario2 = colorimetriaMatrix.tNeutros[indexBasicTerciarios2];
-    if (highlights1 === terciario2)
-      return colorimetriaMatrix.tNeutrosRes[indexBasicTerciarios2];
-
-    // Is Mix highlights2 with tSecundarios?
-    const tSecundario = colorimetriaMatrix.tSecundarios[indexBasicTerciarios];
-    if (highlights2 === tSecundario)
-      return colorimetriaMatrix.tSecundariosRes[indexBasicTerciarios];
-    // Lo mismo pero al reves
-    const tSecundario2 = colorimetriaMatrix.tSecundarios[indexBasicTerciarios2];
-    if (highlights1 === tSecundario2)
-      return colorimetriaMatrix.tSecundariosRes[indexBasicTerciarios2];
-
-    // Is Mix highlights2 with tYernarios?
-    const tYernario = colorimetriaMatrix.tYernarios[indexBasicTerciarios];
-    if (highlights2 === tYernario)
-      return colorimetriaMatrix.tYernariosRes[indexBasicTerciarios];
-    // Lo mismo pero al reves
-    const tYernario2 = colorimetriaMatrix.tYernarios[indexBasicTerciarios2];
-    if (highlights1 === tYernario2)
-      return colorimetriaMatrix.tYernariosRes[indexBasicTerciarios2];
-
-    // Is Mix highlights2 with tYernarios2?
-    const tYernario2Fila2 =
-      colorimetriaMatrix.tYernarios2[indexBasicTerciarios];
-    if (highlights2 === tYernario2Fila2)
-      return colorimetriaMatrix.tYernariosRes[indexBasicTerciarios];
-    // Lo mismo pero al reves
-    const tYernarioFila2 =
-      colorimetriaMatrix.tYernarios2[indexBasicTerciarios2];
-    if (highlights1 === tYernarioFila2)
-      return colorimetriaMatrix.tYernariosRes[indexBasicTerciarios2];
-
-    // Is Mix highlights2 with tMismos?
-    const tMismo = colorimetriaMatrix.tMismos[indexBasicTerciarios];
-    if (highlights2 === tMismo)
-      return colorimetriaMatrix.tMismosRes[indexBasicTerciarios];
-    // Lo mismo pero al reves
-    const tMismo2 = colorimetriaMatrix.tMismos[indexBasicTerciarios2];
-    if (highlights1 === tMismo2)
-      return colorimetriaMatrix.tMismosRes[indexBasicTerciarios2];
+  if (!colorResultado) {
+    console.log({
+      numero: null,
+      color: "Indefinido",
+      grados: gradosResultado,
+      profundidad: 0,
+      coloresSumados: [color1, color2],
+    });
+    throw new Error("No se puede sumar estos colores");
   }
 
-  throw new Error(
-    `Invalid highlights, not found in matrix highlights1:${highlights1}, highlights2:${highlights2}`
-  );
+  return colorResultado.numero;
 }
 
 /**
@@ -300,7 +180,7 @@ function test(expected, arr) {
   const result = processFormula(arr);
   let msg = result + " = " + expected;
   msg += " " + (result === expected ? "✅" : "❌");
-  // console.log(msg);
+  console.log(msg);
 }
 
 test("Ideal Formula: 7.02", [6.32, 8.2]);
@@ -329,3 +209,9 @@ test("Ideal Formula: 0.02", [0.9, 0.2]); // TODO: Que deberia de dar? .0 calido
 test("Ideal Formula: 0.07", [0.5, 0.7]); // TODO: Que deberia de dar? .0 frio
 
 test("Ideal Formula: 0.07", [0.5, 0.1]); // TODO: Que deberia de dar? .5 frio
+
+export default {
+  colores,
+  processPairs,
+  getColor,
+};
